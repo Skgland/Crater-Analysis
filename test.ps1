346f9977df98bd56d8b43501dcee9ddddae54ad6
Bennet Bleßmann <bb-github@t-online.de>
2025-04-26 02:07:08 +0200

    adjust test script to write a log file

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-cargo run --release -- $(Get-ChildItem -Path results -Directory | Select-Object -expand Name)
+cargo run --release -- $(Get-ChildItem -Path results -Directory | Select-Object -expand Name) > output.log
